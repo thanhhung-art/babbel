396cb616d5c54e902fea997fc172655fd2cb79b1
thanhhung <thanhhungdeveloper@gmail.com>
2024-09-07 06:28:01 +0700

    chore: Remove search component from Navbar

--- src/screens/home/Navbar.tsx
+++ src/screens/home/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import BellIcon from "../../assets/icons/BellIcon";
 import Avatar from "../../components/Avatar";
-import Search from "../../components/Search";
 import useAppStore from "../../lib/zustand/store";
 import { verifyUser } from "../../lib/react_query/queries";
 
@@ -16,9 +15,6 @@ const Navbar = () => {
     <nav className="border p-4 flex justify-between items-center">
       <h2>Chat app</h2>
       <ul className="flex gap-8 items-center">
-        <li>
-          <Search />
-        </li>
         <li>
           <BellIcon w={40} h={40} />
         </li>
