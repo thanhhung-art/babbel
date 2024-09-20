3e15776ea8e7aec339997b3f1e488f9358599a54
thanhhung <thanhhungdeveloper@gmail.com>
2024-09-20 17:43:01 +0700

    Add DeleteIcon component for deleting items

---
+++ src/assets/icons/DeleteIcon.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+const DeleteIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g
+      stroke="#ef4444"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    >
+      <path d="M10 11v6M14 11v6M4 7h16M6 7h12v11a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3V7ZM9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2H9V5Z" />
+    </g>
+  </svg>
+);
+export default DeleteIcon;
