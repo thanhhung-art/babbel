126e9db57a47b6f53953e627100e8313b4866670
thanhhung <thanhhungdeveloper@gmail.com>
2024-09-21 18:10:09 +0700

    Add UpdateIcon component for updating items

---
+++ src/assets/icons/UpdateIcon.tsx
@@ -0,0 +1,14 @@
+import { SVGProps } from "react"
+const UpdateIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#3b82f6"
+    stroke="#3b82f6"
+    className="icon glyph"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M18 18H8a1 1 0 0 1-1-1v-4.59l1.29 1.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-3-3a1.15 1.15 0 0 0-.33-.21.94.94 0 0 0-.76 0 1.15 1.15 0 0 0-.33.21l-3 3a1 1 0 0 0 1.42 1.42L5 12.41V17a3 3 0 0 0 3 3h10a1 1 0 0 0 0-2ZM21.71 10.29a1 1 0 0 0-1.42 0L19 11.59V7a3 3 0 0 0-3-3H6a1 1 0 0 0 0 2h10a1 1 0 0 1 1 1v4.59l-1.29-1.3a1 1 0 0 0-1.42 1.42l3 3a1.15 1.15 0 0 0 .33.21.94.94 0 0 0 .76 0 1.15 1.15 0 0 0 .33-.21l3-3a1 1 0 0 0 0-1.42Z" />
+  </svg>
+)
+export default UpdateIcon
