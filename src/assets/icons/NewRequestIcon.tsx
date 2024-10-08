2e293642dcf20a2640fb5018b75e2abf129d7f09
thanhhung <thanhhungdeveloper@gmail.com>
2024-10-09 04:36:07 +0700

    Add NewRequestIcon component for displaying a new request icon

---
+++ src/assets/icons/NewRequestIcon.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from "react";
+const NewRequestIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+    <path
+      d="M3.5 16a.489.489 0 0 1-.191-.038A.5.5 0 0 1 3 15.5V13h-.5A2.5 2.5 0 0 1 0 10.5v-8A2.5 2.5 0 0 1 2.5 0h11A2.5 2.5 0 0 1 16 2.5v8a2.5 2.5 0 0 1-2.5 2.5H6.707l-2.853 2.854A.5.5 0 0 1 3.5 16Zm-1-15A1.5 1.5 0 0 0 1 2.5v8A1.5 1.5 0 0 0 2.5 12h1a.5.5 0 0 1 .5.5v1.793l2.146-2.147A.5.5 0 0 1 6.5 12h7a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 13.5 1ZM9 9a1 1 0 1 0-1 1 1 1 0 0 0 1-1Zm1-4a2 2 0 0 0-4 0 .5.5 0 0 0 1 0 1 1 0 1 1 1 1 .5.5 0 0 0 0 1 2 2 0 0 0 2-2Z"
+      data-name="Path 49"
+    />
+  </svg>
+);
+export default NewRequestIcon;
