17d9e470f2a6abb9f764d0dd88ec992591a65ba4
thanhhung <thanhhungdeveloper@gmail.com>
2025-02-02 16:32:52 +0700

    Add BannedIcon and MenuIcon components for enhanced iconography

---
+++ src/assets/icons/MenuIcon.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+const MenuIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
+export default MenuIcon;
