be7c7557223f854851ae282252aac2e2b45e46c0
thanhhung <thanhhungdeveloper@gmail.com>
2025-02-19 14:54:59 +0700

    Implement registration functionality with error handling and user feedback

--- src/screens/register/Register.tsx
+++ src/screens/register/Register.tsx
@@ -1,38 +1,80 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { registerQuery } from "../../lib/react_query/queries/user/user";
 
 const Register = () => {
   const email = useRef<HTMLInputElement>(null);
   const name = useRef<HTMLInputElement>(null);
   const password = useRef<HTMLInputElement>(null);
   const passwordConfirmation = useRef<HTMLInputElement>(null);
+  const [errors, setErrors] = useState<{
+    email: string;
+    name: string;
+    password: string;
+    passwordConfirmation: string;
+  }>({ email: "", name: "", password: "", passwordConfirmation: "" });
+  const navigate = useNavigate();
+
+  const registerMutation = useMutation({
+    mutationFn: () => {
+      return registerQuery(
+        email.current?.value as string,
+        name.current?.value as string,
+        password.current?.value as string
+      );
+    },
+    onSuccess: () => {
+      navigate("/login");
+    },
+    onError: (error) => {
+      if (error.message === "User with this email already exists")
+        setErrors((prev) => ({
+          ...prev,
+          email: "User with this email already exists",
+        }));
+    },
+  });
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:3000/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email.current?.value,
-        name: name.current?.value,
-        password: password.current?.value,
-      }),
+    setErrors({
+      email: "",
+      name: "",
+      password: "",
+      passwordConfirmation: "",
     });
 
-    const data = await response.json();
+    if (password.current?.value !== passwordConfirmation.current?.value) {
+      setErrors((prev) => ({
+        ...prev,
+        passwordConfirmation: "Passwords do not match",
+      }));
+      return;
+    }
 
-    if (response.ok) {
-      console.log(data);
-    } else {
-      console.log;
+    if (
+      email.current?.value &&
+      name.current?.value &&
+      password.current?.value &&
+      passwordConfirmation.current?.value
+    ) {
+      registerMutation.mutate();
     }
   };
 
+  const handleResetErrors = () => {
+    setErrors({
+      email: "",
+      name: "",
+      password: "",
+      passwordConfirmation: "",
+    });
+  };
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300 px-4">
       <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
         <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">
           register Account
@@ -69,8 +111,13 @@ const Register = () => {
                   ref={email}
                   className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
                   placeholder="E-Mail Address"
+                  required
+                  onFocus={handleResetErrors}
                 />
               </div>
+              {errors.email && (
+                <span className="text-red-500 text-xs">{errors.email}</span>
+              )}
             </div>
 
             <div className="flex flex-col mb-6">
@@ -102,8 +149,14 @@ const Register = () => {
                   ref={name}
                   className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
                   placeholder="Username"
+                  required
+                  onFocus={handleResetErrors}
+                  minLength={4}
                 />
               </div>
+              {errors.name && (
+                <span className="text-red-500 text-xs">{errors.name}</span>
+              )}
             </div>
 
             <div className="flex flex-col mb-6">
@@ -137,13 +190,19 @@ const Register = () => {
                   ref={password}
                   className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
                   placeholder="Password"
+                  required
+                  onFocus={handleResetErrors}
+                  minLength={6}
                 />
               </div>
+              {errors.password && (
+                <span className="text-red-500 text-xs">{errors.password}</span>
+              )}
             </div>
 
             <div className="flex flex-col mb-6">
               <label
-                htmlFor="password"
+                htmlFor="password-confirmation"
                 className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
               >
                 Password Confirmation:
@@ -172,8 +231,16 @@ const Register = () => {
                   name="password-confirmation"
                   className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
                   placeholder="Password"
+                  required
+                  onFocus={handleResetErrors}
+                  minLength={6}
                 />
               </div>
+              {errors.passwordConfirmation && (
+                <span className="text-red-500 text-xs">
+                  {errors.passwordConfirmation}
+                </span>
+              )}
             </div>
 
             <div className="flex w-full">
@@ -181,7 +248,7 @@ const Register = () => {
                 type="submit"
                 className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in"
               >
-                <span className="mr-2 uppercase">Regiter</span>
+                <span className="mr-2 uppercase">Register</span>
                 <span>
                   <svg
                     className="h-6 w-6"
