6542dc70205a48292bd7695f903af823fc61b4bd
thanhhung <thanhhungdeveloper@gmail.com>
2025-02-26 19:50:14 +0700

    Add custom hook for room search with debounced input handling

---
+++ src/hooks/room/useSearchRooms.tsx
@@ -0,0 +1,40 @@
+import { useMutation } from "@tanstack/react-query";
+import { IRoomSearchQuery } from "../../types/room";
+import { useRef } from "react";
+import { searchRoomByNameQuery } from "../../lib/react_query/queries/room/room";
+
+interface IProps {
+  inputValue: React.MutableRefObject<HTMLInputElement | null>;
+}
+
+const useSearchRooms = ({ inputValue }: IProps) => {
+  const deboundSearchRef = useRef<NodeJS.Timeout | null>(null);
+
+  const searchRoomsMutation = useMutation<IRoomSearchQuery[]>({
+    mutationFn: async () => {
+      return await searchRoomByNameQuery(inputValue.current?.value || "");
+    },
+    onError: (error) => {
+      console.error("Error searching rooms:", error);
+    },
+    onSuccess: (data) => {
+      console.log("Search results:", data);
+    },
+  });
+
+  const handleSendSearch = async () => {
+    if (inputValue.current && inputValue.current.value.length > 0) {
+      clearTimeout(deboundSearchRef.current || 0);
+
+      deboundSearchRef.current = setTimeout(() => {
+        searchRoomsMutation.mutate();
+      }, 500);
+    } else {
+      searchRoomsMutation.reset();
+    }
+  };
+
+  return { searchRoomsMutation, handleSendSearch };
+};
+
+export default useSearchRooms;
