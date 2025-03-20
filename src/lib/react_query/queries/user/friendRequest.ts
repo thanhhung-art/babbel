6344d1899ec5769d4952f1e83e6a5f8adde518a7
thanhhung <thanhhungdeveloper@gmail.com>
2025-03-20 18:33:21 +0700

    Refactor friend request query functions for improved readability and consistency

--- src/lib/react_query/queries/user/friendRequest.ts
+++ src/lib/react_query/queries/user/friendRequest.ts
@@ -34,25 +34,7 @@ export async function getFriendRequestQuery(): Promise<
     throw new Error(res.statusText);
   }
 
-  return res.json();
-}
-
-export async function getRequestFriendQuery(): Promise<
-  { id: string; name: string; avatar: string | null }[]
-> {
-  const res = await fetch(`${apiUrl}/user/request-friend`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
-
-  if (!res.ok) {
-    throw new Error(res.statusText);
-  }
-
-  return res.json();
+  return await res.json();
 }
 
 export async function deleteFriendRequestQuery(requestId: string) {
